dd2c933c868dc5fd0a9781693a13670d4b729be9
Colin Shanny <cshannyy@gmail.com>
2020-10-27 14:25:40 -0500

    Switch practice with class cubjects

---
+++ Switchstatements/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Switchstatements
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite subject?");
+
+            var subject = Console.ReadLine(); //evaluate a string
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math is mind bending");
+                    break;
+
+                case "english":
+                     Console.WriteLine("I speak English!");
+                    break;
+
+                case "science":
+                    Console.WriteLine("Science is interesting");
+                    break;
+
+                case "art":
+                    Console.WriteLine("Art is a great way to show a creative side");
+                    break;
+
+                case "history":
+                    Console.WriteLine("History wil show what mistakes the furute will hold");
+                    break;
+            }
+
+        }
+    }
+}
